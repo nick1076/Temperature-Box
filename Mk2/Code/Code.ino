19b8137fd2fc5636224e0d97fe916c9951f4b0e2
Nicholas Niles <nicholasniles0@gmail.com>
2024-11-22 12:08:26 -0500

    Mk2 Added

---
+++ Mk2/Code/Code.ino
@@ -0,0 +1,78 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+
+//Libraries
+#include <DHT.h>;
+
+//Constants
+#define DHTPIN 17     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+
+//Variables
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(1, 10);
+  // Display static text
+  display.println("Nicholas");
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(1, 30);
+  // Display static text
+  display.println("Niles");
+  display.display(); 
+  delay(2000);
+}
+
+void loop()
+{
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+
+    temp=(temp*1.8)+32.0;
+    int temp2 =(int)temp;
+    
+    display.clearDisplay();
+
+    display.setTextSize(4);
+    display.setTextColor(WHITE);
+    display.setCursor(40, 18);
+
+    display.println(temp2);
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(90, 14);
+
+    display.println("o");
+    display.display(); 
+    delay(2000); //Delay 2 sec.
+}
+
+   
